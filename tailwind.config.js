02c6658e12152f423d86a237179dc5d8bf69ffd1
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-14 22:41:29 +0200

    cloning.....

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        customSerif: ["Sumana", "Georgia", "Times New Roman", "Times", "serif"],
+      },
+      fontFamily: {
+        customSans: [
+          "GPC",
+          "Arial Narrow",
+          "Helvetica Neue",
+          "Helvetica",
+          "Arial",
+          "Verdana",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
 };
